90130183b297ac6075d87f6b22766f4b7c30a217
Ayodeji Oni <ayodejioni1505@gmail.com>
2023-04-12 13:07:11 +0000

    new

--- python_lists.c
+++ python_lists.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
 struct LIST{
-}
+	int siz;
+	char list[siz];
+} var[3];
 
+int main(void)
+{
+	var[3] = {{6, "mango"}, {10, "pineapple"}, {11, "Watermelon"}};
+
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		printf("%s", var[i].list[1]);
+	}
+	return (0);
+}
